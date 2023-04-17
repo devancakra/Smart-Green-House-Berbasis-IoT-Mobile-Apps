804651097072eaa8c7676dd2291bf6e5d013c4ff
devan <mudrawijaya@gmail.com>
2023-04-18 04:36:49 +0700

    new-updated source code device-2: nodemcu

--- Source Code/Device_NodeMCU/Final_Project_Edspert_Batch10_NodeMCUProject.ino
+++ Source Code/Device_NodeMCU/Final_Project_Edspert_Batch10_NodeMCUProject.ino
@@ -1,11 +1,11 @@
 // WiFi ke Antares menggunakan protokol HTTP
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#define ACCESSKEY "ISI_ACCESSKEY_ANTARES_ANDA"
-#define WIFISSID "ISI_NAMA_WIFI_ANDA"
-#define PASSWORD "ISI_PASSWORD_WIFI_ANDA"
-#define projectName "ISI_NAMA_APPLICATIONS_ANTARES_ANDA"
-#define deviceName "ISI_NAMA_DEVICE_ANTARES_ANDA"
+//#define ACCESSKEY "ISI_ACCESSKEY_ANTARES_ANDA"
+//#define WIFISSID "ISI_NAMA_WIFI_ANDA"
+//#define PASSWORD "ISI_PASSWORD_WIFI_ANDA"
+//#define projectName "ISI_NAMA_APPLICATIONS_ANTARES_ANDA"
+//#define deviceName "ISI_NAMA_DEVICE_ANTARES_ANDA"
 #define serverName "http://platform.antares.id:8080/~/antares-cse/antares-id/"+String(projectName)+"/"+String(deviceName)
 WiFiClient client;
 HTTPClient http;
@@ -13,24 +13,28 @@ String httpRequestData;
 int httpResponseCode;
 
 // Actuator dan Sensor
-#define SW420_PIN D8 // Sensor Getar
-#define BUZZER_PIN D3 // Buzzer
+#define SW420_PIN D8
+#define BUZZER_PIN D3
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,16,2);
-#define SOLENOID_DOORLOCK_PIN D4
+#define RSOLENOID_DOORLOCK_PIN D4
 #include <SPI.h>
 #include <MFRC522.h>
-#define RST_PIN D1
-#define SDA_PIN D2
-MFRC522 mfrc522(SDA_PIN, RST_PIN);
+constexpr uint8_t RST_PIN = D1;
+constexpr uint8_t SDA_PIN = D2;
+#define UID_RFID1 "53 58 AB AC"
+#define UID_RFID2 "D3 FE 82 1D"
+MFRC522 rfid(SDA_PIN, RST_PIN);
+
+// Inisialisasi memulai mengirim NUID
+byte UniqueIDentifier[4];
 
 // Variabel Global
 int vibration; 
 String alarm_keamanan;
-String content = "";
-byte letter;
 String doorstate;
+String accessCode;
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
 
@@ -43,55 +47,76 @@ void ConnectToWiFi() {
     Serial.print(".");
     delay(500);
   }
-  Serial.println("\nTelah terhubung ke "+String(WIFISSID)+"\n\n");
+  Serial.println("\nTelah terhubung ke jaringan milik: "+String(WIFISSID)+"\n\n");
 }
 
 // Method untuk inisialisasi sensor RFID
 void initSensorRFID(){
   SPI.begin();
-  mfrc522.PCD_Init();
-  Serial.println("Put your card to the reader...");
+  rfid.PCD_Init();
 }
 
-// Method untuk baca sensor
-void bacaSensorGetar(){
-  vibration = digitalRead(SW420_PIN);
-}
+// Method untuk baca sensor RFID
 void bacaSensorRFID(){
-  if(!mfrc522.PICC_IsNewCardPresent()){ return; }
-  if(!mfrc522.PICC_ReadCardSerial()){ return; }
-
-  Serial.print("UID tag :");
-  for(byte i = 0; i < mfrc522.uid.size; i++){
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  // Cek untuk kartu baru
+  if(!rfid.PICC_IsNewCardPresent()) { 
+    return;
+  }
+  // Verifikasi apakah UID sudah dibaca
+  if(!rfid.PICC_ReadCardSerial()) { 
+    return;
+  }
+  if (rfid.uid.uidByte[0] != UniqueIDentifier[0] || rfid.uid.uidByte[1] != UniqueIDentifier[1] || rfid.uid.uidByte[2] != UniqueIDentifier[2] || rfid.uid.uidByte[3] != UniqueIDentifier[3]) {
+    // Store NUID into array
+    for (byte i = 0; i < 4; i++) {
+      UniqueIDentifier[i] = rfid.uid.uidByte[i];
+      accessCode.concat(String(UniqueIDentifier[i] < 0x10 ? " 0" : " "));
+      accessCode.concat(String(UniqueIDentifier[i], HEX));
+    } accessCode.toUpperCase();
+    Serial.print(F("\nUID tag :"));
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+    if(accessCode.substring(1) == UID_RFID1){
+      Serial.print("\nPesan RFID : "); Serial.println("Akses Buka Pintu Green House Berhasil"); doorstate = "Open"; 
+      Display_LCD("Stt.Pintu:"+String(doorstate)); Serial.println("Status Pintu: "+doorstate); 
+      digitalWrite(RSOLENOID_DOORLOCK_PIN, HIGH); delay(1000);
+    }
+    else if(accessCode.substring(1) == UID_RFID2){
+      Serial.print("\nPesan RFID : "); Serial.println("Akses Tutup Pintu Green House Berhasil"); doorstate = "Closed"; 
+      Display_LCD("Stt.Pintu:"+String(doorstate)); Serial.println("Status Pintu: "+doorstate); 
+      digitalWrite(RSOLENOID_DOORLOCK_PIN, LOW); delay(1000);
+    }  
+    else{
+      Serial.print("\nPesan RFID : "); Serial.println("Akses Green House Gagal/UID Belum Terdaftar"); doorstate = "Closed"; 
+      Display_LCD("Stt.Pintu:"+String(doorstate)); Serial.println("Status Pintu: "+doorstate); 
+      digitalWrite(RSOLENOID_DOORLOCK_PIN, LOW); delay(1000);
+    }
+  }
+  else {
+    Serial.print("\nPesan RFID : "); Serial.println("Akses Green House Steril\n"); doorstate = "Closed"; 
+    Display_LCD("Stt.Pintu:"+String(doorstate)); Serial.println("Status Pintu: "+doorstate); 
+    digitalWrite(RSOLENOID_DOORLOCK_PIN, LOW); delay(1000);
   }
 }
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+  // Hentikan PICC
+  rfid.PICC_HaltA();
+  // Hentikan enkripsi pada PCD
+  rfid.PCD_StopCrypto1();
+}
 
-// Method untuk mengolah nilai sensor dan mengatur otomatisasi aktuator
-void Olah_Data(){
-  bacaSensorGetar(); // Memanggil method bacaSensor
-  bacaSensorRFID(); // Memanggil method bacaSensorRFID
-  if(vibration == HIGH){
-    digitalWrite(BUZZER_PIN, HIGH);
-    alarm_keamanan = "ON";
-    Serial.println("Status buzzer = "+String(alarm_keamanan)+" - Danger"); 
-  } else {
-    digitalWrite(BUZZER_PIN, LOW);
-    alarm_keamanan = "OFF";
-    Serial.println("Status buzzer = "+String(alarm_keamanan)+" - Safe");
-  }
-  Serial.print("\nPesan RFID : "); content.toUpperCase();
-  if(content.substring(1) == "D9 DF 60 B9"){
-    Serial.println("Kartu cocok\n"); doorstate = "Open"; Serial.println("Status Pintu: "+doorstate); delay(1000);
-  }
-  else if(content.substring(1) == "79 D0 24 A3"){
-    Serial.println("Kartu cocok\n"); doorstate = "Open"; Serial.println("Status Pintu: "+doorstate); delay(1000);
-  }
-  else{
-    Serial.println("Kartu Tidak cocok"); doorstate = "Closed"; Serial.println("Status Pintu: "+doorstate); delay(1000);
+// Method untuk baca sensor SW-420
+void bacaSensorSW420(){
+  vibration = digitalRead(SW420_PIN);
+  if(vibration == HIGH){ // Jika terdeteksi getaran maka:
+    digitalWrite(BUZZER_PIN, HIGH); // Buzzer: ON
+    alarm_keamanan = "ON"; Serial.println("Status buzzer = "+String(alarm_keamanan)+" - Danger"); // Status Bahaya
+  } else { // Jika tidak ada getaran maka:
+    digitalWrite(BUZZER_PIN, LOW); // Buzzer: OFF
+    alarm_keamanan = "OFF"; Serial.println("Status buzzer = "+String(alarm_keamanan)+" - Safe"); // Status Aman
   }
 }
 
@@ -99,24 +124,29 @@ void Olah_Data(){
 void LCDinit(){
   lcd.init(); // Memulai LCD
   lcd.backlight(); delay(250); lcd.noBacklight(); delay(250); lcd.backlight(); // Splash Screen
-  lcd.setCursor(0,0); lcd.print("Smart GreenHouse"); delay(3000); // Menampilkan data pada LCD
+  lcd.setCursor(0,0); lcd.print("Smart GreenHouse"); lcd.setCursor(4,1); lcd.print("Device-2");delay(3000); // Menampilkan data pada LCD
   lcd.clear(); // Menghapus penampilan data pada LCD 
 }
 void Display_LCD(String responRFID){
-  lcd.setCursor(0,0); lcd.print(responRFID); // Cetak respon RFID di baris 0, kolom 0
+  lcd.setCursor(0,0); lcd.print("INFO -----------");
+  lcd.setCursor(0,1); lcd.print(responRFID); // Cetak respon RFID di baris 1, kolom 0
+}
+
+// Method untuk mengolah nilai sensor dan mengatur otomatisasi aktuator
+void Olah_Data(){
+  bacaSensorRFID(); // Memanggil method bacaSensorRFID
+  bacaSensorSW420(); // Memanggil method bacaSensorSW420
 }
 
 // Kirim Data ke Antares
 void kirimDataAntares(){
+  Olah_Data(); // Memanggil method Olah_Data
   if ((millis() - lastTime) > timerDelay) {
-    Olah_Data(); // Memanggil method Olah_Data
     if (WiFi.status() == WL_CONNECTED) {
       http.begin(client, serverName);
-  
       http.addHeader("X-M2M-Origin",ACCESSKEY);
       http.addHeader("Content-Type","application/json;ty=4");
       http.addHeader("Accept","application/json");
-  
       httpRequestData += "{\"m2m:cin\": { \"con\":\"{\\\"Alarm Keamanan\\\":\\\"";
       httpRequestData += String(alarm_keamanan);
       httpRequestData += "\\\",\\\"Status Pintu\\\":\\\"";
@@ -124,12 +154,10 @@ void kirimDataAntares(){
       httpRequestData += "\\\"}\"}}";
       // Serial.println(httpRequestData);
       // Serial.println();
-      
       int httpResponseCode = http.POST(httpRequestData);
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
       Serial.println("\n");
-  
       http.end();
     }
     else {
@@ -140,14 +168,14 @@ void kirimDataAntares(){
   delay(5000);
 }
 
-// Method ini hanya akan dikerjakan sekali setiap device dinyalakan
-void setup(){
+// Method untuk mengatur inisiasi awal
+void setup() {
   Serial.begin(115200); // Baudrate
   pinMode(SW420_PIN,INPUT); // Inisialisasi pin sw-420 sebagai INPUT
   pinMode(BUZZER_PIN,OUTPUT); // Inisialisasi pin buzzer sebagai OUTPUT
   digitalWrite(BUZZER_PIN, LOW); // Default buzzer: OFF
-  pinMode(SOLENOID_DOORLOCK_PIN,OUTPUT); // Inisialisasi pin solenoid door lock sebagai OUTPUT
-  digitalWrite(SOLENOID_DOORLOCK_PIN, LOW); // Default solenoid door lock: OFF
+  pinMode(RSOLENOID_DOORLOCK_PIN,OUTPUT); // Inisialisasi pin solenoid door lock sebagai OUTPUT
+  digitalWrite(RSOLENOID_DOORLOCK_PIN, LOW); // Default solenoid door lock: OFF (Mengunci)
   ConnectToWiFi(); // Memanggil method ConnectToWiFi
   LCDinit(); // Memanggil method LCDinit
   initSensorRFID(); // Memanggil method initSensorRFID
@@ -155,7 +183,7 @@ void setup(){
 
 // Method ini akan dikerjakan berulang kali
 void loop(){
-  kirimDataAntares();
+  kirimDataAntares(); // Memanggil method kirimDataAntares
 }
 
 // Nama Final Project : Smart Green House (Device-2: NodeMCU)
